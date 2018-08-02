3bee6cebcf209cfd38af1f482e6ce00e794f6cb8
antoniopetruzzella <a.petruzzella71@gmail.com>
2018-08-02 15:54:13 +0200

    init

---
+++ antonio_get_WS/antonio_get_WS.ino
@@ -0,0 +1,52 @@
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+//needed for library
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
+#include <ESP8266HTTPClient.h>
+
+
+void setup() {
+    // put your setup code here, to run once:
+    Serial.begin(115200);
+
+    //WiFiManager
+    //Local intialization. Once its business is done, there is no need to keep it around
+    WiFiManager wifiManager;
+    //reset saved settings
+    //wifiManager.resetSettings();
+    
+    //set custom ip for portal
+    //wifiManager.setAPStaticIPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
+
+    //fetches ssid and pass from eeprom and tries to connect
+    //if it does not connect it starts an access point with the specified name
+    //here  "AutoConnectAP"
+    //and goes into a blocking loop awaiting configuration
+    wifiManager.autoConnect("AutoConnectAP");
+    //or use this for auto generated name ESP + ChipID
+    //wifiManager.autoConnect();
+
+    
+    //if you get here you have connected to the WiFi
+    Serial.println("connected...yeey :)");
+    
+    
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+     delay(10000);
+      HTTPClient http;
+      http.begin("http://www.heritagexperience.com/raspberryWS/raspberryloaddataWS.php?action=insertdata&descrizione=primadawemos&valueInt=233");
+      int httpCode = http.GET(); 
+      if (httpCode > 0) { //Check the returning code
+ 
+      String payload = http.getString();   //Get the request response payload
+      Serial.println(payload);                     //Print the response payload
+ 
+    }
+ 
+    http.end(); 
+// Serial.println(WiFi.localIP());
+}
