0cca5a34dced8c26c895949d02a1f9498eae50f1
antoniopetruzzella <a.petruzzella71@gmail.com>
2018-09-26 22:09:08 +0200

    MW-pulsante per ordine prototipo

---
+++ MARKETWALL/BUTTON/Button/Button.ino
@@ -0,0 +1,79 @@
+/*
+  Button
+
+  Turns on and off a light emitting diode(LED) connected to digital pin 13,
+  when pressing a pushbutton attached to pin 2.
+
+  The circuit:
+  - LED attached from pin 13 to ground
+  - pushbutton attached to pin 2 from +5V
+  - 10K resistor attached to pin 2 from ground
+
+  - Note: on most Arduinos there is already an LED on the board
+    attached to pin 13.
+
+  created 2005
+  by DojoDave <http://www.0j0.org>
+  modified 30 Aug 2011
+  by Tom Igoe
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/Button
+*/
+
+// constants won't change. They're used here to set pin numbers:
+const int buttonPin = D3;     // the number of the pushbutton pin
+const int ledPin =  D4;      // the number of the LED pin
+
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
+
+void setup() {
+Serial.begin(9600);
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+  //digitalWrite(buttonPin, LOW);
+  //digitalWrite(ledPin, LOW);
+}
+
+void loop() {
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+    Serial.println("pulsante rilasciato");
+    
+  } else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
+    Serial.println("pulsante premuto");
+    bool result=verifica3sec();
+    if(result==true){
+      Serial.println ("faccio partire l'ordine");
+    }else{
+      Serial.println("ordine non partito");
+    }
+  }
+  delay(1000);
+}
+
+bool verifica3sec(){
+
+  int i=0;
+  while(i<3){
+
+    if(digitalRead(buttonPin)==HIGH){
+      return false;
+    }
+    i++;
+    delay(1000);
+  }
+  return true;
+}
+
