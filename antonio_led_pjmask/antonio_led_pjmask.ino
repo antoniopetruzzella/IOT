d84227e0230e26e5f84dce2413c3d189626e5f66
antoniopetruzzella <a.petruzzella71@gmail.com>
2018-12-20 10:50:34 +0100

    pjmask

--- antonio_led_pjmask/antonio_led_pjmask.ino
+++ antonio_led_pjmask/antonio_led_pjmask.ino
@@ -1,4 +1,9 @@
+#include <ArduinoJson.h>
 #include <Adafruit_NeoPixel.h>
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+#include <DNSServer.h>
+#include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
+#include <ESP8266HTTPClient.h>
 #define PIN D6
 #define NUMPIXELS 60
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
@@ -10,40 +15,133 @@ struct Pjmask{
   int green;
   int blue;
   };
+HTTPClient httpclient;  
 bool applicationState=1;
 int brightness=125;
-int lumxcent=5;
-Pjmask pjmasks[3];  
+Pjmask pjmasks[4];
+int Showid,Brightness,Speed;
+String Pjmaskname,Frompjmask,Topjmask;
+DynamicJsonBuffer jsonBuffer;
+  
 
 void setup()  {
   Serial.begin(9600);
+  WiFiManager wifiManager;
+  wifiManager.autoConnect("AutoConnectAP");
+  Serial.println("connected...yeey :)");
+  Serial.println(WiFi.localIP()); 
   pjmasks[0].nome="gufetta";pjmasks[0].red=255;pjmasks[0].green=0;pjmasks[0].blue=0;
   pjmasks[1].nome="geco";pjmasks[1].red=0;pjmasks[1].green=255;pjmasks[1].blue=0;
   pjmasks[2].nome="gattoboy";pjmasks[2].red=0;pjmasks[2].green=0;pjmasks[2].blue=255;
+  pjmasks[3].nome="connection_error";pjmasks[3].red=102;pjmasks[3].green=51;pjmasks[3].blue=0;
+  Showid=0;
+  Pjmaskname="0";
+  Frompjmask="0";
+  Topjmask="0";
+  Brightness=100;
+  Speed=1000;
   pixels.begin();
   pixels.show();
   pixels.setBrightness(brightness);
   Serial.println("fine setup");
 }
 
+
 void loop() {
+  /* if (WiFi.status() != WL_CONNECTED){
+    staticPjmask("connection_error",255);
+   }*/
   if(applicationState==1){
-    
-     Serial.println(String(lumxcent));
-    //fadingPjs("gattoboy","gufetta",25,200);
-    staticPjmask("geco",lumxcent);
-    //fade(255,0,0,0,255,0,50,100);     //r1, g1, b1, r2, g2 ,b2 , fade rate , steps
-    //fadeAll(0,255,0,255,0,0,50,100);
+
+    //fadingPjs(pjmaskname,"gufetta",_speed,200);
+    //staticPjmask("gattoboy",lumxcent);
+    //pathPjmask(pjmaskname,_speed,_bidir);
+    //fillingPjmask(pjmaskname,_speed,_bidir);
+    //deflatingPjmask(pjmaskname,_speed,_bidir);
+    //fantasy();
+    playCurrentShow();
+ 
+ 
   }else{
     stopAllShows();
   }
-  lumxcent=lumxcent+20;
-  if(lumxcent>100){
-    lumxcent=100;
-  }
-  delay (1000);
+  delay(Speed);
 }
 
+void playCurrentShow(){
+  
+  int httpcode;
+  int showid;
+  String pjmaskname;
+  int brightness;
+  int _speed_;
+  String frompjmask;
+  String topjmask;
+  while(httpcode!=HTTP_CODE_OK){
+  httpclient.begin("http://www.heritagexperience.com/pjmask/controller.php?action=getcurrentshow");
+                       
+  httpcode=httpclient.GET();
+  //Serial.println("httpcode: "+String(httpcode));
+  //getCurrentShow_=getJsonObjectToParse(httpclient.getString());
+ JsonObject& root = jsonBuffer.parseObject(httpclient.getString().c_str());
+ 
+  const char* _showid=root["showid"];
+  const char* _pjmaskname=root["pjmaskname"];
+  const char* _brightness=root["brightness"];
+  const char* _speed=root["speed"];
+  const char* _frompjmask=root["frompjmask"];
+  const char* _topjmask=root["topjmask"];
+  
+  showid=(String(_showid)).toInt();
+  pjmaskname=String(_pjmaskname);
+  brightness=(String(_brightness)).toInt();
+  _speed_=(String(_speed)).toInt();
+  frompjmask=String(_frompjmask);
+  topjmask=String(_topjmask);
+   
+  if(showid!=Showid || pjmaskname!=Pjmaskname || brightness!=Brightness || _speed_!=Speed || frompjmask!=Frompjmask || topjmask!= Topjmask){
+
+    Showid=showid;
+    Pjmaskname=pjmaskname;
+    Frompjmask=frompjmask;
+    Topjmask=topjmask;
+    Brightness=brightness;
+    Speed=_speed_;
+    stopAllShows();
+  }
+  
+  
+  
+  Serial.println("showid "+showid);
+  httpclient.end();
+
+  }
+  
+  switch(showid){
+
+    case 1:
+  
+      staticPjmask(pjmaskname,brightness);
+      break;
+    
+    }
+  
+}
+
+String getStaticPjmaskname(){
+
+  int httpcode;
+  String pjmaskname;
+  while(httpcode!=HTTP_CODE_OK){
+  httpclient.begin("http://www.heritagexperience.com/pjmask/controller.php?action=getstaticpjmask");
+  httpcode=httpclient.GET();
+  pjmaskname=httpclient.getString();
+  httpclient.end();
+  
+  }
+   
+  return pjmaskname;
+}
 
 void stopAllShows(){
 
@@ -55,12 +153,82 @@ void stopAllShows(){
 }
 
 void staticPjmask(String PjmaskName, int _lumxcent){
+  
   Pjmask pjmask=findPjmaskFromName(PjmaskName);
   for (int x = 0; x < NUMPIXELS; x=x+int(100/_lumxcent)) {
       pixels.setPixelColor(x, pjmask.red, pjmask.green, pjmask.blue);
     }
     pixels.show();
    
+}
+
+void pathPjmask(String PjmaskName, int _speed, bool bidir){
+  Pjmask pjmask=findPjmaskFromName(PjmaskName);
+  for (int x=0; x<NUMPIXELS; x++){
+    pixels.setPixelColor(x, 0, 0, 0);
+  }
+  for (int x=0; x<NUMPIXELS; x++){
+    pixels.setPixelColor(x, pjmask.red, pjmask.green, pjmask.blue);
+    if(x>0){
+      pixels.setPixelColor(x-1, 0, 0, 0);
+    }
+    pixels.show();
+    delay(_speed);
+  }
+  if(bidir==true){
+    for (int x=NUMPIXELS; x>0; x--){
+      pixels.setPixelColor(x, pjmask.red, pjmask.green, pjmask.blue);
+      if(x>0){
+        pixels.setPixelColor(x+1, 0, 0, 0);
+      }
+      pixels.show();
+      delay(_speed);
+    }
+  }
+  
+}
+
+void fillingPjmask(String PjmaskName, int _speed, bool bidir){
+  Pjmask pjmask=findPjmaskFromName(PjmaskName);
+  for (int x=0; x<NUMPIXELS; x++){
+    pixels.setPixelColor(x, 0, 0, 0);
+  }
+  pixels.show();
+  for (int x=0; x<NUMPIXELS; x++){
+    pixels.setPixelColor(x, pjmask.red, pjmask.green, pjmask.blue);
+    pixels.show();
+    delay(_speed);
+  }
+  if(bidir==true){
+    for (int x=NUMPIXELS; x>0; x--){
+      pixels.setPixelColor(x, 0, 0, 0);
+      pixels.show();
+      delay(_speed);
+    }
+  }
+  
+}
+
+void deflatingPjmask(String PjmaskName, int _speed, bool bidir){
+  Pjmask pjmask=findPjmaskFromName(PjmaskName);
+  for (int x=0; x<NUMPIXELS; x++){
+    pixels.setPixelColor(x, pjmask.red, pjmask.green, pjmask.blue);
+  }
+  pixels.show();
+  for (int x=NUMPIXELS; x>0; x--){
+      pixels.setPixelColor(x, 0, 0, 0);
+      pixels.show();
+      delay(_speed);
+    }
+  if(bidir==true){
+    for (int x=0; x<NUMPIXELS; x++){
+    pixels.setPixelColor(x, pjmask.red, pjmask.green, pjmask.blue);
+    pixels.show();
+    delay(_speed);
+    }
+   
+  }
+  
 }
 void fadingPjs(String startPjmaskName, String endPjmaskName, int vel, int passi){
   
@@ -80,12 +248,20 @@ Pjmask findPjmaskFromName(String _nome){
   }
 }
 
+void fantasy(){
+  int r1=125;int g1=178; int b1=253; int r2=12; int g2=78; int b2=99;
+  fade(r1,g1,b1,r2,g2,b2,5,50);
+}
+
 void fade(int r1, int g1, int b1, int r2, int g2, int b2, int fadeRate, int steps) {
+  uint8_t red;
+  uint8_t green;
+  uint8_t blue;
   for (int i = 1; i < steps; i++)
   {
-    uint8_t red = (((r1 * (steps - i)) + (r2 * i)) / steps);
-    uint8_t green = (((g1 * (steps - i)) + (g2 * i)) / steps);
-    uint8_t blue = (((b1 * (steps - i)) + (b2 * i)) / steps);
+    red = (((r1 * (steps - i)) + (r2 * i)) / steps);
+    green = (((g1 * (steps - i)) + (g2 * i)) / steps);
+    blue = (((b1 * (steps - i)) + (b2 * i)) / steps);
     // Sets the pixels to the color adjusted in the fade
     for (int x = 0; x < NUMPIXELS; x++) {
       pixels.setPixelColor(x, red, green, blue);
@@ -93,4 +269,5 @@ void fade(int r1, int g1, int b1, int r2, int g2, int b2, int fadeRate, int step
     pixels.show();
     delay(fadeRate);
   }
+  delay(5000);
 }
